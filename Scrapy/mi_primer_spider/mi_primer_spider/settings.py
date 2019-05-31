8d7dd35bbd8f03fc7c9e7477d0d8874b7f33e5c2
mlaricobar <bcpcoelab@MacBook-Pro-de-BCP.local>
2019-05-31 17:34:40 -0500

    Agregando modulo de Scrapy

--- Scrapy/mi_primer_spider/mi_primer_spider/settings.py
+++ Scrapy/mi_primer_spider/mi_primer_spider/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'mi_primer_spider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'mi_primer_spider (+http://www.yourdomain.com)'
+USER_AGENT = 'mi_primer_spider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- Scrapy/mi_primer_spider/mi_primer_spider/spiders/example.py
+++ Scrapy/mi_primer_spider/mi_primer_spider/spiders/example.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+class LinioSpider(scrapy.Spider):
+	name = 'linio_spider'
+	allowed_domains = ['linio.com.pe']
+	start_urls = ['https://www.linio.com.pe/c/celulares-y-tablets/']
 
-class ExampleSpider(scrapy.Spider):
-    name = 'example'
-    allowed_domains = ['example.com']
-    start_urls = ['http://example.com/']
-
-    def parse(self, response):
-        pass
+	def parse(self, response):
+		product_titles = response.xpath("//div[@id='catalogue-product-container']//div[@class='detail-container']//span[@class='title-section']/text()").extract()
+		yield {'product_titles': product_titles}
